33f19ef97f84a3cb99b8d6765fcfb52316356802
Abhishek <abhishekgautam1966@gmail.com>
2025-08-23 17:27:40 +0530

    handle errors

--- index.js
+++ index.js
@@ -15,35 +15,47 @@ app.use(cors(corsOptions))
 
 makeDbConnection()
 
+const allProducts = async () => {
+    try{
+        const products = await ItemInfo.find()
+        return products
+    }catch(error){
+        console.log(error)
+    }
+}
 
 app.get("/products", async (req, res) => {
     try{
-        const allProducts = await ItemInfo.find()
-        res.status(200).json(allProducts)
+        const petProducts = await allProducts()
+        if(petProducts){
+            res.status(200).json(petProducts)
+        }else{
+            res.status(404).json({error: "products not found."})
+        }
     }catch(error){
-        res.status(500).json({message : error.message})
+        res.status(500).json({error: error.message})
     }    
 })
 
-app.get("/products/:productId" , async (req, res) => {
-    try{
-        const id = req.params.productId
-        const selectedProduct = await ItemInfo.findById(id)
-        res.status(200).json(selectedProduct)
-    }catch(error){
-        res.status(500).json({message : error.message})
-    }
-})
+// app.get("/products/:productId" , async (req, res) => {
+//     try{
+//         const id = req.params.productId
+//         const selectedProduct = await ItemInfo.findById(id)
+//         res.status(200).json(selectedProduct)
+//     }catch(error){
+//         res.status(500).json({message : error.message})
+//     }
+// })
 
-app.get("/categories", async (req, res) => {
-    try{
-        const allCategories = (await (ItemInfo.find())).map(prod => prod.category)
-        const finalCategories = [...new Set(allCategories)]
-        res.status(200).json(finalCategories)
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+// app.get("/categories", async (req, res) => {
+//     try{
+//         const allCategories = (await (ItemInfo.find())).map(prod => prod.category)
+//         const finalCategories = [...new Set(allCategories)]
+//         res.status(200).json(finalCategories)
+//     }catch(error){
+//         res.status(500).json({message: error.message})
+//     }
+// })
 
 const PORT = 3000
 
